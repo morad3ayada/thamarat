fe7e28305ff8bfe8a9634d30755de2bb1bde5d0e
morad3ayada <morad3ayada@gmail.com>
2025-06-17 17:10:07 +0300

    done type

--- lib/data/models/materials_model.dart
+++ lib/data/models/materials_model.dart
@@ -39,17 +39,33 @@ class MaterialsModel {
 
   // Helper getter for display type
   String get displayType {
-    switch (materialType) {
-      case 'consignment':
-        return 'صافي وزن';
-      case 'markup':
-        return 'خابط وزن';
-      case 'spoiledConsignment':
-        return 'صافي عدد';
-      case 'spoiledMarkup':
-        return 'خابط عدد';
-      default:
-        return materialType;
+    print('Material Type: $materialType, Is Quantity: $isQuantity'); // Debug log
+    if (isQuantity) {
+      switch (materialType) {
+        case 'consignment':
+          return 'صافي عدد';
+        case 'markup':
+          return 'خابط عدد';
+        case 'spoiledConsignment':
+          return 'صافي عدد';
+        case 'spoiledMarkup':
+          return 'خابط عدد';
+        default:
+          return 'صافي عدد';
+      }
+    } else {
+      switch (materialType) {
+        case 'consignment':
+          return 'صافي وزن';
+        case 'markup':
+          return 'خابط وزن';
+        case 'spoiledConsignment':
+          return 'صافي وزن';
+        case 'spoiledMarkup':
+          return 'خابط وزن';
+        default:
+          return 'صافي وزن';
+      }
     }
   }
 
